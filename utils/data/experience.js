40fa034b7a3db22dcaa50f8ed792eee597c80a2a
shiva52523 <149316402+shiva52523@users.noreply.github.com>
2025-09-03 14:44:23 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,12 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software development',
+    title: 'Python Developer Intern',
     company: "AD Infocom System",
-    duration: "(Jan 2024 - July 2024)"
+    duration: "(Jan 2025 - July 2025)"
   },
   {
     id: 2,
-    title: "Data Analyst Intern",
-    company: "Bayslope Biusiness Solution",
-    duration: "(Aug 2024 - Dec 2024)"
-  },
-  {
-    id: 3,
     title: "Python & AI Developer",
     company: "Shilsha Technologies",
     duration: "(Jul 2025 - Present)"
