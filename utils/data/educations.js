6adaf5d8cfaf02dc29fbd487b797f0e7929db97b
shiva52523 <149316402+shiva52523@users.noreply.github.com>
2025-09-03 14:48:39 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,19 +1,19 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
+    title: "Bachelor of Technology",
     duration: "2021 - 2025",
     institution: "Dr. A P J Abdul Kalam Technical University Lucknow UP",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
+    title: "Intermediate",
     duration: "2019 - 2021",
     institution: "UP Board",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
+    title: "High School",
     duration: "2016 - 2018",
     institution: "Kunwar Convent School Ballia UP",
   }
