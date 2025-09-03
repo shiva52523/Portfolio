b56a4a7bbe262c26a081c89efa77e45869cf86ce
shiva52523 <149316402+shiva52523@users.noreply.github.com>
2025-09-03 15:04:33 +0530

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -2,77 +2,122 @@ export const skillsData = [
   'HTML',
   'CSS',
   'Javascript',
-  'Typescript',
   'React',
   'Next JS',
-  'Nuxt JS',
   'Node JS',
-  'Vue',
-  'Angular',
-  'Svelte',
-  'ViteJS',
-  'VuetifyJS',
-  'Tailwind',
   'Bootstrap',
-  'Bulma',
-  'CapacitorJs',
-  'MaterialUI',
-  'Strapi',
-  'Graphql',
   'MongoDB',
   'MySQL',
   'PostgreSQL',
-  'Prisma',
-  'MemSQL',
   'Git',
   'Docker',
-  'Nginx',
   'AWS',
   'GCP',
   'Azure',
   'Firebase',
   'Fastify',
-  'Deno',
   'Django',
   'PHP',
   'Python',
   'Java',
   'C',
   'C++',
-  'C#',
-  'Go',
-  'Ruby',
-  'Kotlin',
-  'Swift',
-  'Dart',
-  'Flutter',
-  'Julia',
-  'Matlab',
-  'Haxe',
-  'Ionic',
-  'Markdown',
-  'Wordpress',
-  'Unity',
   'Tensorflow',
   'Pytorch',
   'Numpy',
   'OpenCV',
   'Selenium',
-  'WolframAlpha',
-  'Webex',
-  'Photoshop',
-  'Illustrator',
-  'Figma',
   'Canva',
-  'Sketch',
-  'Blender',
-  'Premiere Pro',
-  'After Effects',
-  'Lightroom',
-  'Adobe XD',
-  'Adobe Audition',
-  'Picsart',
   'Microsoft Office',
-  'Gimp',
-  'Coffeescript'
 ]
+
+
+
+
+// Choose your skills from below. Make sure it's in the same format and spelled correctly.
+// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+
+
+// AVAILABLE SKILLS
+
+/* 
+  HTML
+  CSS
+  JS 
+  React
+  Next JS
+  Nuxt JS
+  Node JS
+  Vue
+  Angular
+  Docker
+  Photoshop
+  Illustrator
+  Svelte
+  GCP
+  Azure
+  Fastify
+  Haxe
+  Ionic
+  Markdown
+  Microsoft Office
+  Picsart
+  Sketch
+  Unity
+  WolframAlpha
+  Adobe XD
+  After Effects
+  Bootstrap
+  Bulma
+  CapacitorJs
+  Coffeescript
+  MemSQL
+  C
+  C++
+  C#
+  Python
+  Java
+  Julia
+  Matlab
+  Swift
+  Ruby
+  Kotlin
+  Go
+  PHP
+  Flutter
+  Dart
+  Typescript
+  Swift
+  Git
+  Figma
+  Canva
+  Ubuntu
+  Bootstrap
+  MongoDB
+  Tailwind
+  ViteJS
+  VuetifyJS
+  MySQL
+  PostgreSQL
+  Prisma
+  AWS
+  Firebase
+  Blender
+  Premiere Pro
+  Adobe Audition
+  Deno
+  Django
+  Gimp
+  Graphql
+  Lightroom
+  MaterialUI
+  Nginx
+  Numpy
+  OpenCV
+  Pytorch
+  Selenium
+  Strapi
+  Tensorflow
+  Webex
+  Wordpress
+  */
